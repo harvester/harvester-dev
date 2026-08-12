75c228d45c92d110feb65f9af372e1eb54bffbd2
Kiefer Chang <kiefer.chang@suse.com>
2026-08-12 10:36:38 +0800

    fix: don't enable descheduler if node is less than 2
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- op/tests/smoke-addons-ready.sh
+++ op/tests/smoke-addons-ready.sh
@@ -5,6 +5,7 @@ source "$SCRIPT_DIR/../lib.sh"
 
 TOP_DIR=$(get_top_dir)
 SMOKE_DIR=$(get_smoke_dir)
+CONFIG=$(get_config_file)
 KUBECONFIG_FILE=$(get_kubeconfig_file)
 
 if [ ! -f "$KUBECONFIG_FILE" ]; then
@@ -16,6 +17,12 @@ fi
 addons_config="$TOP_DIR/state/addons_config.yaml"
 rm -f "$addons_config"
 cp "$SMOKE_DIR/addons_config.yaml.sample" "$addons_config"
+
+NODE_COUNT=$(yq '.node_count' "$CONFIG")
+if [ "$NODE_COUNT" -lt 2 ]; then
+  echo "Removing descheduler from the addon test configuration because node_count is less than 2"
+  yq -i 'del(.addons[] | select(.namespace == "kube-system" and .name == "descheduler"))' "$addons_config"
+fi
 
 echo "Running smoke test to enable and verify Harvester addons..."
 set -x
