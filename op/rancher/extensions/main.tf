2fda8b084313a261d12e8d14b11aa390247f349f
Kiefer Chang <kiefer.chang@suse.com>
2026-08-14 08:59:28 +0800

    feat: add rancher-ai-ui extension support in configuration and Terraform files
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- op/rancher/extensions/main.tf
+++ op/rancher/extensions/main.tf
@@ -13,9 +13,10 @@ terraform {
 
 
 locals {
-  credentials      = yamldecode(file("../../../state/rancher_bootstrap_credentials.yaml"))
-  config           = yamldecode(file("../../../config.yaml"))
-  harvester_ui_ext = local.config.rancher.extensions.harvester-ui
+  credentials       = yamldecode(file("../../../state/rancher_bootstrap_credentials.yaml"))
+  config            = yamldecode(file("../../../config.yaml"))
+  harvester_ui_ext  = local.config.rancher.extensions.harvester-ui
+  rancher_ai_ui_ext = local.config.rancher.extensions.rancher-ai-ui
 }
 
 provider "rancher2" {
@@ -51,4 +52,26 @@ resource "rancher2_app_v2" "harvester-ui" {
   repo_name = rancher2_catalog_v2.harvester[0].name
   chart_name = "harvester"
   chart_version = local.harvester_ui_ext.version
+}
+
+resource "rancher2_catalog_v2" "rancher-ai-ui" {
+  provider = rancher2.admin
+  count    = local.rancher_ai_ui_ext.enabled ? 1 : 0
+
+  cluster_id = data.rancher2_cluster.local.id
+  name = "rancher-ai-ui"
+  git_repo = local.rancher_ai_ui_ext.git_repo
+  git_branch = local.rancher_ai_ui_ext.git_branch
+}
+
+resource "rancher2_app_v2" "rancher-ai-ui" {
+  provider = rancher2.admin
+  count    = local.rancher_ai_ui_ext.enabled ? 1 : 0
+
+  cluster_id = data.rancher2_cluster.local.id
+  name = "rancher-ai-ui"
+  namespace = "cattle-ui-plugin-system"
+  repo_name = rancher2_catalog_v2.rancher-ai-ui[0].name
+  chart_name = "rancher-ai-ui"
+  chart_version = local.rancher_ai_ui_ext.version
 }
