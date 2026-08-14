9dce8576158285263916e9b183ecdbbfd5ca57fb
Kiefer Chang <kiefer.chang@suse.com>
2026-08-14 17:23:31 +0800

    fix: init terraform in install-extensions script
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- op/rancher/install-extensions.sh
+++ op/rancher/install-extensions.sh
@@ -8,10 +8,16 @@ source "$SCRIPT_DIR/../lib.sh"
 TOP_DIR=$(get_top_dir)
 STATE_DIR=${TOP_DIR}/state
 
+tf_init() {
+    pushd $SCRIPT_DIR > /dev/null
+    terraform -chdir=extensions init
+}
+
 install_rancher_extensions() {
     pushd $SCRIPT_DIR > /dev/null
 
     terraform -chdir=extensions apply --auto-approve
 }
 
+tf_init
 install_rancher_extensions
